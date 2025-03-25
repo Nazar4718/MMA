c20d92ebaa03d4eeb56b5dcae2d7294307d439d9
Nazar <civkanazar@gmail.com>
2025-03-25 17:36:14 +0200

    create two page

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+
+<html>
+@include("partials.head")
+<body>
+@include("partials.header")
+
+    <div>
+        @yield("content")
+    </div>
+    @include("partials.footer")
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends("app")
+
+@section("content")
+homePage
+@endsection
