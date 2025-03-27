c1cd1bc70b11b9048bca958484ec4dd52ca6a0e3
Nazar <civkanazar@gmail.com>
2025-03-27 16:28:26 +0200

    login

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegistrationController;
 
 Route::get("/",[HomeController::class,"index"])->name("home");
 Route::get("/registration", [RegistrationController::class, "index"])->name("registaration");
+Route::get("/login", [LoginController::class, "index"])->name("login");
 
